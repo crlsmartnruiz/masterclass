e2a85979caaaecaae29691fc5045489bb8a90a9f
Carlos Martin <cmartin@gradiant.org>
2021-03-13 17:34:19 +0100

    First commit

---
+++ dockerfile/express/commands.sh
@@ -0,0 +1,7 @@
+docker rm -f express-app
+
+docker build -t express-app .
+
+#docker run -p 3000:3000 -e EXAMPLE_ENV_VAR=example-value -d --name express-app --rm --network js-network express-app
+
+docker run -p 3000:3000 -e EXAMPLE_ENV_VAR=example-value -d --name express-app --rm --volume $PWD:/app/src express-app
